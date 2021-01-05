07a4196771af8ddb65a514726169833e87d8d49e
YUKE WANG <yuke_wang@cs.ucsb.edu>
2021-01-05 10:50:29 -0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,18 +1,3 @@
 from .vgg import *
 from .mobilenet import *
 from .resnet import *
-
-# from .dpn import *
-# from .lenet import *
-# from .senet import *
-# from .pnasnet import *
-# from .densenet import *
-# from .googlenet import *
-# from .shufflenet import *
-# from .shufflenetv2 import *
-# from .resnext import *
-# from .preact_resnet import *
-# from .mobilenetv2 import *
-# from .efficientnet import *
-# from .regnet import *
-# from .rpw_conv import *
