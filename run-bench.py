ee096e41738142203d890fb8e5bcd3f0a5470337
YUKE WANG <yuke_wang@cs.ucsb.edu>
2021-02-24 21:09:44 -0800

    Update run-bench.py

--- run-bench.py
+++ run-bench.py
@@ -2,8 +2,6 @@
 
 import sys
 import os
-import subprocess
-
 
 models = [
     # 'VGG11',
@@ -18,9 +16,8 @@ models = [
 
 groups = [
     # 1,
-    # 2,
-    # 4,
-    8
+    2,
+    # 4
 ]
 
 overlaps = [
@@ -28,10 +25,9 @@ overlaps = [
     0.50,
     # 0.75
 ]
+
 for model in models:
     for grp in groups:
         for oap in overlaps:
             print("=> {}, g: {}, o: {}".format(model, grp, oap))
             os.system("python main.py --model {} --groups {} --overlap {}".format(model, grp, oap))
-            # p = subprocess.Popen("python main.py --model {} --groups {} --overlap {}".format(model, grp, oap), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0].decode('utf-8')
-            # print("time: {}".format(p.split("\n")[-2].split(':')[-1]))
